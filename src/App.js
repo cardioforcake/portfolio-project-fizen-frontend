0a64631fa390200e3a497b5f29d61ac5a6bb135e
cardioforcake <chaos_241@hotmail.com>
2021-08-17 11:54:26 -0700

    add route for landing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path="/tutorial">
             <Tutorial/>
           </Route>
-          <Route>
+          <Route path="/">
             <LandingPage/>
           </Route>
         </Switch>
