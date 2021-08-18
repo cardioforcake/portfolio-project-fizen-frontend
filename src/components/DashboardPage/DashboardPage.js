6218a5c914027a8083744f8505b3d8d2737e25de
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-08-18 17:38:11 -0400

    mock up Goal List and Goal Details

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -1,12 +1,45 @@
+import { useState } from 'react';
+import { Button, List, ListItem, Typography } from '@material-ui/core';
+
+
 export default function DashboardPage({ goals }) {
+  const [zoomedGoal, setZoomedGoal] = useState(null);
+
   return (
     <div>
-      <h1>Dashboard</h1>
-      <h2>My Goals</h2>
-      {
-        goals.length ?
-          goals.map((goal) => <li key={goal._id}>{goal.title}</li>)
-          : <span>no goals</span>
+      {zoomedGoal ?
+        <div>
+          <Button onClick={() => setZoomedGoal(null)}>return to goals</Button>
+          <Typography variant="h6">{zoomedGoal.title}</Typography>
+          <List>
+            <ListItem>description: {zoomedGoal.description}</ListItem>
+            <ListItem>targetDate: {zoomedGoal.targetDate}</ListItem>
+            <ListItem>targetAmount: {zoomedGoal.targetAmount}</ListItem>
+            <ListItem>currentAmount: {zoomedGoal.currentAmount}</ListItem>
+            <ListItem>riskTolerance: {zoomedGoal.riskTolerance}</ListItem>
+            <ListItem>isReached: {zoomedGoal.isReached}</ListItem>
+          </List>
+        </div>
+        :
+        <div>
+          <Typography variant="h3">My Goals</Typography>
+          <List>
+            {
+              goals.length ?  goals.map((goal) => {
+                return (
+                  <ListItem
+                    button
+                    key={goal._id}
+                    onClick={() => setZoomedGoal(goal)}
+                  >
+                    {goal.title}
+                  </ListItem>
+                );
+              })
+              : <ListItem component="div">No goals!</ListItem>
+            }
+          </List>
+        </div>
       }
     </div>
   );
